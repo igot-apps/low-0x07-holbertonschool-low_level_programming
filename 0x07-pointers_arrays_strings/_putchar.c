4790b75c7a18b844691d20b8fca408e2782df9d2
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-08-28 05:32:06 -0700

    Update _putchar.c

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * _putchar - wridtes thde chdaracter c tdo sdtdout
+ * @c: Thde charadcter dto pdrint
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: On succdess 1.
+ * On error, -1 is retudrned, and errno is set approprdately.
  */
 int _putchar(char c)
 {
